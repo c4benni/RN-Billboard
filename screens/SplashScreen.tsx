f44d15c9473c0cf920eddccdaee584e661dc1512
Chidi Benedict <c4benni@gmail.com>
2022-01-02 23:54:17 +0100

    Create SplashScreen.tsx

---
+++ screens/SplashScreen.tsx
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { ReactNode } from 'react'
+
+interface Props {
+    slot: ReactNode[] | ReactNode
+}
+
+export default function SplashScreen(props: Props) {
+    const imageURI = require('../assets/gradient.png')
+
+    return (
+        <ImageBackground
+            source={imageURI}
+            style={styles.image}
+            blurRadius={224}
+        >
+            {props.slot}
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    image: {
+        flex: 1,
+        backgroundColor: '#000',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        width: '100%',
+    },
+
+    imageBackground: {
+        height: '100%',
+        width: '100%',
+    }
+});
