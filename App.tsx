639c5230cd6ce185faa32eae46f801082b004f6c
Chidi Benedict <c4benni@gmail.com>
2022-01-03 18:29:30 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
+import ChartsScreen from './screens/ChartsScreen';
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
+  const isAndriod = Platform.OS == 'android';
+  const isIOS = Platform.OS == 'ios'
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -15,7 +18,23 @@ export default function App() {
           component={HomeScreen}
           options={{ headerShown: false }}
         />
-        {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+        <Stack.Screen
+          name="Charts"
+          component={ChartsScreen}
+          options={{
+            headerStyle: {
+              backgroundColor: 'transparent',
+            },
+            headerTitleStyle: {
+              color: '#fff'
+            },
+            headerShadowVisible: isAndriod,
+            headerTransparent: isIOS,
+            headerTintColor: '#000',
+            // headerBlurEffect: 'systemChromeMaterialDark',
+            title: ''
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
