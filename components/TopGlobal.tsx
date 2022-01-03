21a662d1b7871b6ecd1a9da44e1efcc1a608799b
Chidi Benedict <c4benni@gmail.com>
2022-01-03 21:02:36 +0100

    Update TopGlobal.tsx

--- components/TopGlobal.tsx
+++ components/TopGlobal.tsx
@@ -6,11 +6,12 @@ import MiniChartCard from './MiniChartCard';
 
 interface Props {
     windowWidth: number,
+    divideBottom?: boolean
 }
 
 const TopGlobal = (props: Props) => {
 
-    const { windowWidth } = props;
+    const { windowWidth, divideBottom } = props;
 
     const screenPercent = windowWidth > 600 ? 0.25 : 0.5
 
@@ -23,7 +24,10 @@ const TopGlobal = (props: Props) => {
     const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     return (
-        <HomeSection title='Global top 200' children={
+        <HomeSection
+            title='Global top 200'
+            divideBottom={divideBottom}
+            children={
             <CardCarousel
                 length={list.length}
                 itemWidth={cardWidth}
@@ -35,7 +39,8 @@ const TopGlobal = (props: Props) => {
                                 return (
                                     <MiniChartCard
                                         key={i}
-                                        title='Easy on me' src={{ uri: 'https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/73/6d/7c/736d7cfb-c79d-c9a9-4170-5e71d008dea1/886449666430.jpg/400x400cc.jpg' }} subtitle={`Adele-${i}`}
+                                        title='Easy on me'
+                                        src={{ uri: 'https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/73/6d/7c/736d7cfb-c79d-c9a9-4170-5e71d008dea1/886449666430.jpg/400x400cc.jpg' }} subtitle={`Adele-${i}`}
                                         style={{
                                             width: cardWidth,
                                             marginRight: i == a.length - 1 ? gap * 2 : gap,
