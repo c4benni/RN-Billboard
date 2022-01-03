5c2f8208a03a377ab4fe87fc8bb8847d8b3a00c2
Chidi Benedict <c4benni@gmail.com>
2022-01-03 21:02:54 +0100

    Update HomeScreen.tsx

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { ScrollView, StyleSheet, useWindowDimensions } from 'react-native'
-import SplashScreen from './SplashScreen';
+import { SafeAreaView, ScrollView, StyleSheet, Text, useWindowDimensions, View } from 'react-native'
+// import SplashScreen from './SplashScreen';
 
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import TopGlobal from '../components/TopGlobal';
+import FeaturedArtists from '../components/FeaturedArtists';
 
 type RootStackParamList = {
     Home: undefined;
@@ -16,17 +17,31 @@ const HomeScreen = ({ navigation }: Props) => {
 
     const { width: windowWidth } = useWindowDimensions();
 
+    // return (
+    //     <SplashScreen
+    //         slot={
+                
+    //         }
+    //     />
+    // );
+
     return (
-        <SplashScreen
-            slot={
-                <ScrollView style={[
-                    styles.root
-                ]}>
-                    <TopGlobal windowWidth={windowWidth} />
-                </ScrollView>
-            }
-        />
-    );
+        <SafeAreaView style={[
+            styles.root
+        ]}>           
+            <ScrollView>
+                <View style={styles.titleWrap}>
+                    <Text style={styles.title}>
+                        Browse
+                    </Text>
+                </View>
+                
+                <TopGlobal windowWidth={windowWidth} divideBottom />
+
+                <FeaturedArtists windowWidth={windowWidth} />
+            </ScrollView>
+        </SafeAreaView>
+    )
 };
 
 export default HomeScreen
@@ -36,5 +51,19 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 72,
         paddingBottom: 96,
+        backgroundColor:'#111'
     },
+    titleWrap: {
+        borderBottomColor: '#222',
+        borderBottomWidth: 0.75,
+        paddingBottom: 8,
+        marginHorizontal: 16,
+        marginTop: 24
+    },
+    title: {
+        fontSize: 36,
+        fontWeight: '700',
+        textAlign: 'left',
+        color:'#fff'
+    }
 })
