571bb5baaa48a91aa94f56d1f01689b953e3ad04
Chidi Benedict <c4benni@gmail.com>
2022-01-05 20:49:09 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,39 +4,49 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Platform } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import ChartsScreen from './screens/ChartsScreen';
+import { StatusBar } from 'expo-status-bar';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+// import UserList from './UserList';
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
   const isAndriod = Platform.OS == 'android';
   const isIOS = Platform.OS == 'ios'
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Charts"
-          component={ChartsScreen}
-          options={{
-            headerStyle: {
-              backgroundColor: 'transparent',
-            },
-            headerTitleStyle: {
-              color: '#fff'
-            },
-            headerShadowVisible: isAndriod,
-            headerTransparent: isIOS,
-            headerTintColor: '#000',
-            // headerBlurEffect: 'systemChromeMaterialDark',
-            title: ''
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <StatusBar translucent />
+      {/* <UserList /> */}
+
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Charts"
+            component={ChartsScreen}
+            options={{
+              headerStyle: {
+                backgroundColor: 'transparent',
+              },
+              headerTitleStyle: {
+                color: '#fff'
+              },
+              headerShadowVisible: isAndriod,
+              headerTransparent: isIOS,
+              headerTintColor: '#000',
+              // headerBlurEffect: 'systemChromeMaterialDark',
+              title: ''
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
