7ea512baad23af8e7f2248f45e912745cb75f940
Chidi Benedict <c4benni@gmail.com>
2022-01-05 20:48:47 +0100

    Update MiniChartCard.tsx

--- components/MiniChartCard.tsx
+++ components/MiniChartCard.tsx
@@ -1,5 +1,6 @@
 import { ElementType } from 'react'
-import { StyleSheet, TouchableOpacity, TouchableNativeFeedback, Platform, Text, Image, } from 'react-native'
+import { StyleSheet, TouchableOpacity, TouchableNativeFeedback, Platform, Text, Image, View, } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
 import PressEffect from './PressEffect'
 
 interface Props {
@@ -7,8 +8,9 @@ interface Props {
     onPress?: Function,
     title: string,
     subtitle: string,
-    src: {
-        uri: string
+    img: {
+        src: string,
+        alt: string
     },
     imageHeight?: number,
     imageStyle?: object,
@@ -19,27 +21,50 @@ interface Props {
 
 const MiniChartCard = (props: Props) => {
 
-    const { style, title, subtitle, src, imageHeight, imageStyle, subTitleStyle, titleStyle } = props
+    const { style, title, subtitle, img, imageHeight, imageStyle, subTitleStyle, titleStyle } = props
+
+    const imageStyles = [styles.image, {
+        height: imageHeight || 164,
+        ...imageStyle
+    }]
 
     return (
         <PressEffect
             style={[style]}
         >
-            <Image
-                source={src}
-                style={[styles.image, {
-                    height: imageHeight || 164,
-                    ...imageStyle
-                }]}
-            />
+            {
+                img.src ? 
+                    <Image
+                        accessibilityLabel={img.alt}
+                        source={{
+                            uri: img.src,
+                            cache: 'force-cache'
+                        }}
+                        style={imageStyles}
+                    />
+                    : <View
+                        accessibilityLabel='music icon'
+                        style={[imageStyles, {
+                            backgroundColor: '#333'
+                        }]}
+                    >
+                        <Icon
+                            name='ios-musical-notes'
+                            size={64}
+                            color={'#777'}
+                        />
+                    </View>
+            }
             <Text
                 style={[styles.title, titleStyle]}
+                numberOfLines={1}
             >
                 {title}
             </Text>
 
             <Text
                 style={[styles.subtitle, subTitleStyle]}
+                numberOfLines={1}
             >
                 {subtitle}
             </Text>
@@ -66,16 +91,21 @@ const styles: style = StyleSheet.create({
     image: {
         width: '100%',
         borderRadius: 4,
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     title: {
         fontWeight: '500',
         fontSize: 15,
         color: '#fff',
-        marginTop: 4
+        marginTop: 4,
+        paddingRight: 1
     },
     subtitle: {
         color: '#fff',
         fontSize: 14,
-        opacity: 0.7
+        opacity: 0.7,
+        paddingRight: 1
     }
 })
