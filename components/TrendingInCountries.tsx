393b2b1ed65ea8079db1e44d26930e30261ff23c
Chidi Benedict <c4benni@gmail.com>
2022-01-05 20:48:31 +0100

    Update TrendingInCountries.tsx

--- components/TrendingInCountries.tsx
+++ components/TrendingInCountries.tsx
@@ -1,8 +1,12 @@
-import React, { Fragment } from 'react'
-import { StyleSheet } from "react-native";
+import React, { Fragment, useEffect } from 'react'
+import { StyleSheet, Text } from "react-native";
 import CardCarousel from './CardCarousel';
 import HomeSection from './HomeSection';
 import ChartCountries from './ChartCountries';
+import { RootState } from '../redux/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchResult } from '../redux/slice/homeTrendingInSlice';
+import { HomeTrendingIn } from '../types/homeScreen';
 
 interface Props {
     windowWidth: number,
@@ -11,49 +15,69 @@ interface Props {
 
 const TrendingInCountries = (props: Props) => {
 
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchResult())
+    }, [])
+
+    const trendingIn = useSelector((state: RootState) => state.homeTrendingIn);
+
+    if (trendingIn.loading) {
+        return (
+            <Text>
+                Loading
+            </Text>
+        )
+    }
+
+    // return null if fetchResults has an error 
+    // or there's no result;
+    if (trendingIn.error || !trendingIn.result.length) {
+        return null
+    }
+
     const { windowWidth, divideBottom } = props;
 
     const screenPercent = windowWidth > 600 ? 0.50 : 1
 
-    const visibleOffset = screenPercent > 0.50 ? 40 : 20
+    const visibleOffset = screenPercent > 0.50 ? 30 : 20
 
     const cardWidth = (windowWidth * screenPercent) - visibleOffset
 
-    const gap = screenPercent > 0.30 ? 10 : 16;
+    const gap = screenPercent > 0.50 ? 8 : 16;
 
-    const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const list = trendingIn.result;
+
+    const divide = divideBottom ? {
+        paddingBottom: 16,
+        borderBottomWidth: 1,
+        borderBottomColor: '#222',
+    } : {}
 
     return (
         <HomeSection
             title='Trending in...'
-            divideBottom={divideBottom}
+            divideBottom={false}
             children={
                 <CardCarousel
                     length={list.length}
                     itemWidth={cardWidth}
                     gap={gap}
                     decelerationRate={0.1}
-                    children={
-                        <Fragment>
-                            {
-                                list.map((_, i, a) => {
-                                    return (
-                                        <ChartCountries
-                                            key={i}
-                                            title='USA'
-                                            subtitle='Featuring songs from Years & Years, Bon Iver, Fireboy DML & Ed Sheeran and more'
-                                            src={{ uri: 'https://is1-ssl.mzstatic.com/image/thumb/Features125/v4/a2/2d/df/a22ddf71-9254-043c-c162-11fbc25c5ff3/mzl.ilbjswky.jpg/800x800cc.jpg' }}
-                                            style={{
-                                                width: cardWidth,
-                                                marginRight: i == a.length - 1 ? gap * 2 : gap,
-                                                marginLeft: i == 0 ? gap * 2 : 0
-                                            }}
-                                        />
-                                    )
-                                })
-                            }
-                        </Fragment>
-                    } />
+                    data={list}
+                    render={({ item, index }: { item: HomeTrendingIn, index: number }) => (<ChartCountries
+                        title={item.name}
+                        subtitle={item.featuring}
+                        src={{ uri: 'https://is1-ssl.mzstatic.com/image/thumb/Features125/v4/a2/2d/df/a22ddf71-9254-043c-c162-11fbc25c5ff3/mzl.ilbjswky.jpg/800x800cc.jpg' }}
+                        style={{
+                            width: cardWidth,
+                            marginRight: index == list.length - 1 ? gap * 2 : gap,
+                            marginLeft: index == 0 ? gap * 2 : 0,
+                            ...divide
+                        }}
+                    />)}
+                />
             } />
     )
 
