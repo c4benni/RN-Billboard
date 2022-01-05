a6f4340d3bfa1893989853f503098c23dbc2cffa
Chidi Benedict <c4benni@gmail.com>
2022-01-05 20:47:42 +0100

    Create homeTrendingInSlice.ts

---
+++ redux/slice/homeTrendingInSlice.ts
@@ -0,0 +1,30 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import fetchHomeTrendingIn from "../../services/homeTrendingIn";
+
+import { HomeTopGlobal } from "../../types/homeScreen";
+import { HomeTrendingIn } from "../../types/redux";
+import { fetchBuilder, resultFetcher } from "../utils";
+
+const initialState: HomeTrendingIn = {
+  loading: false,
+  error: false,
+  result: [],
+};
+
+export const fetchResult = createAsyncThunk<{ result?: HomeTopGlobal[] }>(
+  "fetchHomeTrendingIn",
+  async () =>
+    await resultFetcher(
+      fetchHomeTrendingIn,
+      "error fetching fetchHomeTrendingIn"
+    )
+);
+
+const homeTrendingIn = createSlice({
+  name: "homeTrendingIn",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => fetchBuilder(builder, fetchResult as any),
+});
+
+export default homeTrendingIn.reducer;
